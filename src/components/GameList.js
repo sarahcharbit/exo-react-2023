0bd37150dcdd6f1cb96d813b7de5133f3f4ee5b8
sarahcharbit <sarahcharbit000@gmail.com>
2023-02-10 19:05:20 +0100

    filter

--- src/components/GameList.js
+++ src/components/GameList.js
@@ -3,8 +3,10 @@ import { useEffect, useState } from "react";
 import Game from './Game';
 
 const GameList = () => {
-    
+    const [filteredGames, setFilteredGames]= useState(false)
     const [gameAllLists, setGameAllLists]= useState ([])
+    
+    function clickButton() {setFilteredGames(!filteredGames)}
     
     useEffect(() => {
         axios
@@ -13,16 +15,25 @@ const GameList = () => {
         .then ((res) => setGameAllLists(res))
          
     }, [])
-return (
-        <div className="containerGameList"> {gameAllLists.map((gameAllList)=> {
-            return (
-                    <div>
-                        <div> Name: {gameAllList.name}</div>
-                        <div> <Game released={gameAllList.released} rating={gameAllList.rating}/></div>
-                    </div>
-                   )
-        })}
+    
+    return (
+        <div className="containerGameList">
+            {gameAllLists
+                .filter((gameAllList)=> filteredGames || gameAllList.rating >= 4.5)
+                .map((gameAllList, index) => {
+                    return (
+                        <div>
+                            <div> Name: {gameAllList.name}</div>
+                            <div> <Game key={index} released={gameAllList.released} rating={gameAllList.rating}/></div>
+                        </div>
+                    )         
+                })
+            }
+            <button onClick={clickButton}>
+                {''}
+                {filteredGames ? 'show only games rating more 4': 'show all games'}
+            </button>
         </div>
-        )
+    )
 }
 export default GameList
